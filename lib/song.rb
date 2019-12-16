d655aad1f6922c331b09bd690689883c7986f838
Meagan Utsey <meagan.utsey@gmail.com>
2019-12-16 01:53:49 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -11,3 +11,87 @@ class Song
   end
 
 end
+s = Song.new
+s.name = "The Ship Song"
+s.artist_name = "Nick Cave"
+s.save
+end 
+
+ attr_accessor :name, :artist_name
+  @@all = []
+
+  def song.create 
+    @@all
+    song = Song.create
+Song.all.include?(song) #=> true
+end 
+attr_accessor :name, :artist_name
+  @@all = []
+
+  def song.new_by_name 
+    @@all
+song = Song.new_by_name("The Middle")
+#=> #<Song @name="The Middle">
+song.name #=> "The Middle"
+end 
+attr_accessor :name, :artist_name
+  @@all = []
+
+  def song.create_by_name 
+    @@all
+    song = Song.create_by_name("The Middle")
+#=> #<Song:0x007fd2a2989ff0 @name="The Middle">
+song
+#=> #<Song:0x007fd2a2989ff0 @name="The Middle">
+Song.all.include?(song)
+#=> true
+end 
+attr_accessor :name, :artist_name
+  @@all = []
+
+  def song.find_by_name 
+    @@all
+    the_middle = Song.create_by_name("The Middle")
+#=> #<Song @name="The Middle">
+ 
+Song.find_by_name("The Middle")
+#<Song @name="The Middle">
+end 
+attr_accessor :name, :artist_name
+  @@all = []
+
+  def song.find_or_create_by_name 
+    @@all
+song_1 = Song.find_or_create_by_name("Blank Space")
+song_2 = Song.find_or_create_by_name("Blank Space")
+ 
+# song_1 and song_2 are conceptually the same song and should return the same song instance because of `.find_or_create_by_name.`
+ 
+song_1 == song_2 #=> true
+end 
+attr_accessor :name, :artist_name
+  @@all = []
+
+  def song.alphabetical 
+    @@all
+    
+attr_accessor :name, :artist_name
+  @@all = []
+
+  def song.new_from_filename
+    @@all    
+    song = Song.new_from_filename("Taylor Swift - Blank Space.mp3")
+song.name #=> "Blank Space"
+song.artist_name #=> "Taylor Swift"
+end 
+attr_accessor :name, :artist_name
+  @@all = []
+
+  def song.create_from_filename
+    @@all  
+    
+  attr_accessor :name, :artist_name
+  @@all = []
+
+  def song.destroy_all 
+    @@all  
